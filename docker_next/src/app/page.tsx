05cfeeb928646863558a7ffacccaa52557accfce
Harish Garg <rishusharma123321@gmail.com>
2025-07-10 10:19:46 +0530

    final changes

---
+++ docker_next/src/app/page.tsx
@@ -0,0 +1,22 @@
+'use client'
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [data, setData] = useState("null");
+
+  useEffect(() => {
+      fetch("http://localhost:3001/backend/data") 
+      .then((res) => res.text())
+      .then((text) => setData(text))
+      
+  }, []);
+
+  return (
+    <div>
+      <h1>This is</h1>
+      <pre>{data}</pre>
+      <h2>this </h2>
+
+    </div>
+  );
+}
